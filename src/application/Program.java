5188f11447e51929c0caa36b6c0a69d9b75c3eda
LuCarlosMelo <Luizcapeme@gmail.com>
2022-11-15 21:30:47 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,10 +5,10 @@ import boardgame.Position;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Position position = new Position(3, 5);
 		System.out.println(position);
-		
+
 	}
 
 }
